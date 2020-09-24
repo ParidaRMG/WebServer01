fe70d6bd0c651a5e968d63bf0a1c43bce7efc604
Rishi Parida <rrparida00@gmail.com>
2020-09-24 19:02:40 -0400

    Fixed formatting

--- Prog535-1.py
+++ Prog535-1.py
@@ -16,7 +16,7 @@ class myHandler(BaseHTTPRequestHandler):
           return 0
 
       #This variable will count the number of requests
-      requestCounter = 0
+      #requestCounter = 0
 
       # Add your code below for various content types
       try:
@@ -27,46 +27,41 @@ class myHandler(BaseHTTPRequestHandler):
          if self.path.endswith(".html"):
             mimetype='text/html'
             #print("HTML text request type at path:",self.path)
-            requestCounter += 1
+            #requestCounter += 1
             sendReply = True
          # add more code here for various image types
          if self.path.endswith(".jpg"):
             mimetype='picture/jpg'
             #print("JPG text request type at path",self.path)
-            requestCounter += 1
+            #requestCounter += 1
             sendReply = True
 
          if self.path.endswith(".png"):
            mimetype='picture/png'
            #print("PNG text request type at path",self.path)
-           requestCounter += 1
+           #requestCounter += 1
            sendReply = True
 
          if self.path.endswith(".gif"):
            mimetype='picture/gif'
            #print("GIF text request type at path",self.path)
-           requestCounter += 1
+           #requestCounter += 1
            sendReply = True
 
          if self.path.endswith(".bmp"):
            mimetype='picture/bmp'
            #print("BMP text request type at path",self.path)
-           requestCounter += 1
+           #requestCounter += 1
            sendReply = True
 
          if sendReply == True:
             #Open the static file requested and send it
             f = open(curdir + sep + self.path, "rb")
-            #print("point1")
             self.send_response(200)
-            #print("point2")
             self.send_header('Content-type',mimetype)
-            #print("point3")
             self.end_headers()
-            #print("point4")
             self.wfile.write(f.read() )
             f.close()
-            #print("\n\n")
             #print("RequestCounter:", requestCounter)
          else:
              print("File:", self.path, " not found")
